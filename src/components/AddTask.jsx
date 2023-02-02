b86364ef919caadae2112722fb47ec427cad5db3
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-02-02 18:03:01 -0300

    Pegando dado do input e adicionando nas tarefas

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Button from "./Button";
 
 import "../css/AddTask.css";
 
 const AddTask = ({ handleTaskAddition }) => {
+  const [inputData, setInputData] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputData(e.target.value);
+  };
+
+  const handleAddTaskClick = () => {
+    handleTaskAddition(inputData);
+    setInputData("");
+  };
+
   return (
     <div className="add-task-container">
       <input
         type="text"
         className="add-task-input"
         placeholder="Descreva a tarefa aqui"
+        onChange={handleInputChange}
+        onKeyUp={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            handleAddTaskClick();
+          }
+        }}
+        value={inputData}
       />
       <div className="add-task-button-container">
-        <Button>Criar tarefa</Button>
+        <Button onClick={handleAddTaskClick}>Criar tarefa</Button>
       </div>
     </div>
   );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "../css/Button.css";
 
-const Button = ({ children }) => {
-  return <button className="button">{children}</button>;
+const Button = ({ children, onClick }) => {
+  return (
+    <button onClick={onClick} className="button">
+      {children}
+    </button>
+  );
 };
 
 export default Button;
